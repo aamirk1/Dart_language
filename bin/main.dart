25aa20c2706974e81c79747bcbb50af3a0a9c07f
unknown <pratyush.com>
2023-05-17 18:57:34 +0530

    three initialize d1

--- bin/main.dart
+++ bin/main.dart
@@ -108,4 +108,28 @@ comment
 // print(l--);
 // print(l);
 
+// ## Equality and Relational Operators
+
+var a= 10;
+var b = 20;
+print(a == b);
+print(a != b);
+print(a > b);
+print(a < b);
+print(a >= b);
+print(a <= b);
+
+// ## Logical Operators
+
+  /* true && true = true;
+    true && false = false;
+    false && true = false;
+    false && false = false;
+
+    true || true = true;
+    true || false = false;
+    false || true = false;
+    false || false = false; */
+
+
 }
