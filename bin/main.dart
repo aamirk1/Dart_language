92b4f3e64476fca3011dfaac8f6b8b6b73133e6d
unknown <pratyush.com>
2023-05-17 18:46:03 +0530

    second initialize d1

--- bin/main.dart
+++ bin/main.dart
@@ -9,24 +9,103 @@ comment
 
 // print("Hello World");
 // # Identifier or Variable Name And Rules
-var name = "dart";
-var f_name = "Aamir";
-var lastname1 = "Khan";
-var $money = 100;
+// var name = "dart";
+// var f_name = "Aamir";
+// var lastname1 = "Khan";
+// var $money = 100;
 
 // ## Invalid Variable Name 
 // var 1persion = "abc";
 // var for = "reserve key word";
 
 // # Print Variable
-print(name);
-print(f_name);
-print(lastname1);
-print($money);
+// print(name);
+// print(f_name);
+// print(lastname1);
+// print($money);
 
 
 // Built in Data type 
 
+// int cart_item =10;
+// print(cart_item);
 
+// double price = 20.50;
+// print(price);
+
+// String  city = "Mumbai";
+// print(city);
+
+// bool is_completed= true;
+// print(is_completed);
+
+// bool is_login = false;
+// print(is_login);
+
+// var quantity = 100.60;
+// print(quantity);
+// print(quantity.runtimeType); // this is used for check datatype of variable
+
+// var quantity = 100;
+// print(quantity);
+// var cost = 10.50;
+// print(cost);
+
+// var gender = "Male";
+// print(gender);
+
+// var is_full = true;
+// print(is_full);
+
+// // check Data Type
+// print(quantity.runtimeType);
+// print(cost.runtimeType);
+// print(gender.runtimeType);
+// print(is_full.runtimeType);
+
+// # Operators
+// ## Arithmatic Operators
+
+// var a = 10;
+// var b = 20;
+// var c = 30;
+// var d = 40;
+
+// print(a + b);
+// print(c - b);
+// print(a * b);
+// print(d / a);
+// print(d ~/ a);
+// print(d % a);
+
+
+// ## Prefix and Postfix Increament and Decrement Operators
+
+// var i = 3;
+// print(i);
+// print(++i);
+// print(i);
+//  OR
+
+// var j = 3;
+// print(j);
+// print(j++);
+// print(j);
+// print("");
+
+// And
+
+
+// var k = 3;
+// print(k);
+// print(--k);
+// print(k);
+
+// OR
+
+// var l = 3;
+// print(l);
+// print(l--);
+// print(l);
 
 }
