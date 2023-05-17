45c0899618d82b95cf29fa6f10fc7703af30d37e
unknown <pratyush.com>
2023-05-17 17:18:56 +0530

    first initialize

---
+++ bin/main.dart
@@ -0,0 +1,32 @@
+void main(){
+  // This is Single Line Comment
+
+/*
+This is 
+Multi line 
+comment
+*/ 
+
+// print("Hello World");
+// # Identifier or Variable Name And Rules
+var name = "dart";
+var f_name = "Aamir";
+var lastname1 = "Khan";
+var $money = 100;
+
+// ## Invalid Variable Name 
+// var 1persion = "abc";
+// var for = "reserve key word";
+
+// # Print Variable
+print(name);
+print(f_name);
+print(lastname1);
+print($money);
+
+
+// Built in Data type 
+
+
+
+}
